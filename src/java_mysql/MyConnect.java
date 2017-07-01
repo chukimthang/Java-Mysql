2978f17f064d3e6b1a508d1005883d0a2cb4a4c8
Chu Kim Thang <chukimthang94@gmail.com>
2017-07-02 01:04:11 +0700

    Create function CRUD

--- src/java_mysql/MyConnect.java
+++ src/java_mysql/MyConnect.java
@@ -7,7 +7,10 @@ package java_mysql;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,17 +20,18 @@ import java.util.logging.Logger;
  */
 public class MyConnect {
 
-    private final String className = "com.mysql.jdbc.Driver";
-    private final String url = "jdbc:mysql://localhost:3306/java_mysql";
-    private final String user = "root";
-    private final String pass = "";
+    public final String className = "com.mysql.jdbc.Driver";
+    public final String url = "jdbc:mysql://localhost:3306/java_mysql";
+    public final String user = "root";
+    public final String pass = "";
 
-    private Connection connection;
-    
+    public String table = "tbl_student";
+    public Connection connection;
+
     /**
      * Connect database
      */
-    private void connect() {
+    public void connect() {
         try {
             Class.forName(className);
             connection = DriverManager.getConnection(url, user, pass);
@@ -38,9 +42,132 @@ public class MyConnect {
             System.out.println("Error connection");
         }
     }
-    
+
+    /**
+     * Show all list data
+     *
+     * @param rs
+     */
+    public void showData(ResultSet rs) {
+        try {
+            while (rs.next()) {
+                System.out.println(rs.getString(1) + " - "
+                        + rs.getString(2) + " - "
+                        + rs.getFloat(3));
+            }
+        } catch (SQLException ex) {
+            System.err.println("Error show data \n" + ex.toString());
+        }
+    }
+
+    /**
+     * Get list data
+     *
+     * @return
+     */
+    public ResultSet getData() {
+        ResultSet rs = null;
+        String sql = "SELECT * FROM " + table;
+        Statement st;
+        try {
+            st = connection.createStatement();
+            rs = st.executeQuery(sql);
+        } catch (SQLException ex) {
+            System.err.println("select error \n" + ex.toString());
+        }
+        return rs;
+    }
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    public ResultSet getDataId(int id) {
+        ResultSet rs = null;
+        String sql = "SELECT * FROM " + table + " WHERE id = ?";
+        try {
+            PreparedStatement pst = connection.prepareStatement(sql);
+            pst.setInt(1, id);
+            rs = pst.executeQuery();
+        } catch (SQLException ex) {
+            System.err.println("Select error \n" + ex.toString());
+        }
+        return rs;
+    }
+
+    /**
+     * Insert record
+     *
+     * @param student
+     */
+    public void insert(Student student) {
+        String sql = "INSERT INTO " + table + " VALUE(?, ?, ?)";
+        try {
+            PreparedStatement pst = connection.prepareStatement(sql);
+            pst.setInt(1, student.getId());
+            pst.setString(2, student.getName());
+            pst.setFloat(3, student.getPoint());
+            if (pst.executeUpdate() > 0) {
+                System.out.println("Insert success!");
+            } else {
+                System.out.println("Insert error!");
+            }
+        } catch (SQLException ex) {
+            System.err.println("Insert error \n" + ex.toString());
+        }
+    }
+
+    /**
+     * Update record
+     *
+     * @param student
+     * @param id
+     */
+    public void update(int id, Student student) {
+        String sql = "UPDATE " + table + " SET name = ?, point = ? WHERE id = ?";
+        try {
+            PreparedStatement pst = connection.prepareStatement(sql);
+            pst.setString(1, student.getName());
+            pst.setFloat(2, student.getPoint());
+            pst.setInt(3, student.getId());
+            if (pst.executeUpdate() > 0) {
+                System.out.println("Update success!");
+            } else {
+                System.out.println("Update error!");
+            }
+        } catch (SQLException ex) {
+            System.err.println("Update error \n" + ex.toString());
+        }
+    }
+
+    /**
+     * Delete record by id
+     *
+     * @param id
+     */
+    public void delete(int id) {
+        String sql = "DELETE FROM " + table + " WHERE id = ?";
+        try {
+            PreparedStatement pst = connection.prepareStatement(sql);
+            pst.setInt(1, id);
+            if (pst.executeUpdate() > 0) {
+                System.out.println("Delete success!");
+            } else {
+                System.out.println("Record not found!");
+            }
+        } catch (SQLException ex) {
+            System.err.println("Delete error \n" + ex.toString());
+        }
+    }
+
     public static void main(String[] args) {
         MyConnect myConnect = new MyConnect();
         myConnect.connect();
+//        myConnect.showData(myConnect.getDataId(1));
+//        myConnect.delete(2);
+//        myConnect.insert(new Student(2, "Lê Đức Phúc", (float) 8.6)); 
+        myConnect.update(3, new Student(3, "Nguyen Manh Quang", (float) 6.5));
+        myConnect.showData(myConnect.getData());
     }
 }
